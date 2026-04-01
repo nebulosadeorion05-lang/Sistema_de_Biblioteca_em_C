ec27020dc7414bcd59157f22d4d0a70f5a103f2e
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2026-04-01 13:11:40 -0300

    Add biblioteca.h for Biblioteca structure and functions

--- Base do Sistema/biblioteca.h
+++ Base do Sistema/biblioteca.h
@@ -1,1 +1,18 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
 
+#include "livro.h"
+
+typedef struct {
+    Livro *livros;
+    int quantidade;
+    int capacidade;
+} Biblioteca;
+
+Biblioteca* criarBiblioteca(int capacidadeInicial);
+void adicionarLivro(Biblioteca *bib, Livro livro);
+void listarLivros(Biblioteca *bib);
+void expandirBiblioteca(Biblioteca *bib);
+void liberarBiblioteca(Biblioteca *bib);
+
+#endif
