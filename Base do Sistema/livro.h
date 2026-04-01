7da2cb1d219bc6073a8fa3858c8556e0f888281f
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2026-04-01 12:42:01 -0300

    Add Livro struct and related functions in livro.h

--- Base do Sistema/livro.h
+++ Base do Sistema/livro.h
@@ -1,1 +1,12 @@
+#ifndef LIVRO_H
+#define LIVRO_H
 
+typedef {
+    int id, ano_de_publicacao, quantidade_de_paginas, ja_lido;
+    char *titulo, *autor, *genero;
+} Livro;
+
+Livro criarLivro(int id, int ano_de_publicacao, int quantidade_de_paginas, int ja_lido, char *titulo, char *autor, char *genero);
+void liberarLivro(Livro *book);
+
+#endif
